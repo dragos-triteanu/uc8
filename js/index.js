feb9c1c737adfcc6dcd0abff750e29ab85787e70
dragos.triteanu <dragos.triteanu@gmail.com>
2016-05-17 13:24:42 +0200

    Implemented mail sending capabilities

--- js/index.js
+++ js/index.js
@@ -9,4 +9,47 @@ $(document).on("ready", function(){
         $(".contact-overlay").hide();
         $(".main-section").fadeIn();
     });
+
+    $("#contact-form").on("submit",function(e){
+        e.preventDefault();
+
+        $("#send-message-button").prop("disabled",true);
+        var name = $("#name-input").val();
+        var email = $("#email-input").val();
+        var message = $("#message-input").val();
+        sendTheMail(name,email,message);
+
+
+        return false;
+    });
+
+
+    function sendTheMail(name,email,message){
+        $.ajax({
+            url: "http://localhost:8089/sendEmail",
+            type: "POST",
+            dataType: "application/x-www-form-urlencoded",
+            data : {
+                name: name,
+                email: email,
+                text: message
+            },
+            // cache: false,
+            success: function(response) {
+                console.log(response);
+            },
+            error: function(response) {
+                console.log(response);
+                $("#send-message-button").prop("disabled",false);
+            },
+            complete: function(){
+                $(".contact-overlay").hide();
+                $(".main-section").fadeIn();
+                $("#name-input").val("");
+                $("#email-input").val("");
+                $("#message-input").val("");
+            }
+        });
+    }
+
 });
